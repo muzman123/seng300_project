013dbcc4c16574ecfb43ed6a49d27807b6459a96
muzman123 <muzammilmuhammad12@gmail.com>
2023-11-02 21:29:01 -0600

    completed PayViaCoin iteration 1

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCoin.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCoin.java
@@ -3,7 +3,10 @@ package com.thelocalmarketplace.software;
 import java.math.BigDecimal;
 
 import com.tdc.coin.Coin;
+import com.tdc.coin.CoinStorageUnit;
+import com.tdc.coin.CoinValidator;
 
+import ca.ucalgary.seng300.simulation.InvalidArgumentSimulationException;
 import ca.ucalgary.seng300.simulation.NullPointerSimulationException;
 
 
@@ -12,23 +15,45 @@ public class PayViaCoin {
 	private BigDecimal AMOUNT_DUE;
 	private BigDecimal AMOUNT_PAID = BigDecimal.ZERO;
 	private boolean amountPaidInFull = false;
+	private CoinValidator coin_validator;
+	private CoinStorageUnit storage_unit;
 	
-	public PayViaCoin(BigDecimal amountDue, Coin payment)
+	public PayViaCoin(BigDecimal amountDue, Coin payment, CoinValidator validator, CoinStorageUnit storage)
 	{
-		AMOUNT_DUE = amountDue;
+		if(amountDue == BigDecimal.ZERO)
+			throw new InvalidArgumentSimulationException("Amount due argument cannot be zero");
 		
-		receiveCoin(payment);
+		AMOUNT_DUE = amountDue;
+		coin_validator = validator;
+		storage_unit = storage;
+		
+		try {
+			receiveCoin(payment);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 	
-	public PayViaCoin(BigDecimal amountDue, Coin[] payment)
+	public PayViaCoin(BigDecimal amountDue, Coin[] payment, CoinValidator validator, CoinStorageUnit storage)
 	{
+		if(amountDue == BigDecimal.ZERO)
+			throw new InvalidArgumentSimulationException("Amount due argument cannot be zero");
+		
 		AMOUNT_DUE = amountDue;
+		coin_validator = validator;
+		storage_unit = storage;
 		
 		for(Coin coin: payment)
-			receiveCoin(coin);
+			try {
+				receiveCoin(coin);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
 	}
 	
-	private void receiveCoin(Coin coin)
+	private void receiveCoin(Coin coin) throws Exception
 	{
 		if (coin == null)
 			throw new NullPointerSimulationException("coin");
@@ -37,15 +62,31 @@ public class PayViaCoin {
 		{
 			AMOUNT_DUE = AMOUNT_DUE.subtract(coin.getValue());
 			AMOUNT_PAID = AMOUNT_PAID.add(coin.getValue());
+			try 
+			{
+				coin_validator.receive(coin);
+				storage_unit.receive(coin);
+			}
+			catch(Exception e)
+			{
+				throw e;
+			}
+				
 			SignalCustomer();
 		}
 		else if (AMOUNT_DUE.equals(BigDecimal.ZERO))
 		{
 			amountPaidInFull = true;
+			PrintReceipt();
 		}
 			
 	}
 	
+	private void PrintReceipt() {
+		// TODO implement print receipt functionality
+		
+	}
+
 	public BigDecimal getAmountDue()
 	{
 		return AMOUNT_DUE;
@@ -63,7 +104,7 @@ public class PayViaCoin {
 	
 	private void SignalCustomer()
 	{
-		//TODO signal customer on gui of updated amount due
+		//TODO signal customer on GUI of updated amount due
 	}
 	
 }
